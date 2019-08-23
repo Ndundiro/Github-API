d91b3eaee6a48b92a6d7de432f770834c9ed58de
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-08-23 18:41:09 +0300

    Creates two files main-page and input-search-form components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { InputSearchFormComponent } from './input-search-form/input-search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    InputSearchFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/input-search-form/input-search-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-search-form',
+  templateUrl: './input-search-form.component.html',
+  styleUrls: ['./input-search-form.component.css']
+})
+export class InputSearchFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
