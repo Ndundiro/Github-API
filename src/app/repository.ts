337c5d156aabbcf76e93cddb7cf0883bb4be0442
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-08-26 01:59:30 +0300

    Adds a class repository and defines some parameters in the constructor.

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,5 +1,7 @@
 export class Repository {
    
-        constructor(public name: string, public description: string, public html_url: string, public  clone_url: string, public homepage:string ) {}
-        
+        constructor(public name: string, public description: string, public html_url: string, public  clone_url: string, public language:string, public homepage:string,public created_at:Date
+                , public forks:number ) {}
+
+   
 }
