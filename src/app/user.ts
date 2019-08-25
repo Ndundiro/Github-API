1cccb7dd7ca685080a8f2cb71ba23360c1e404ac
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-08-26 01:55:07 +0300

    Adds a class user and defines some parameters in the constructor.

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,4 +1,7 @@
 export class User {
-    constructor(public name: string, public description: string, public html_url: string, public  clone_url: string, public homepage:string, avatar_url: any ) {}
+    constructor(public name: string, public html_url: string, public_repos:number, public followers:number, public following:number, public  clone_url: string, public homepage:string,public avatar_url: any ) {}
 }
+
+
+
 
